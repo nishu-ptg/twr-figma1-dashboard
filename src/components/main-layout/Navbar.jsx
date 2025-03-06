f76749691e2c8f5784be0855fc6188aee3f0f8fa
Nishu <nishuptg@yahoo.com>
2025-03-06 19:17:10 +0600

    nav item active bar animation

--- src/components/main-layout/Navbar.jsx
+++ src/components/main-layout/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
     { id: "credit-cards", name: "Credit Cards" },
     { id: "loans", name: "Loans" },
     { id: "services", name: "Services" },
-    { id: "my-priviledges", name: "My Priviledges" },
+    { id: "my-privileges", name: "My Privileges" },
     { id: "settings", name: "Settings" },
   ];
 
@@ -21,7 +21,17 @@ const Navbar = () => {
   };
 
   return (
-    <nav className="flex flex-col py-[10px] space-y-[2px]">
+    <nav className="flex flex-col py-[10px] space-y-[2px] relative">
+      {/* Active Indicator */}
+      <div
+        className="absolute left-[-6px] w-[12px] h-[62px] bg-[#2d60ff] rounded-full transition-all duration-300"
+        style={{
+          transform: `translateY(${
+            navItems.findIndex((item) => item.id === activeItem) * 64
+          }px)`,
+        }}
+      ></div>
+
       {navItems.map((item) => (
         <NavbarItem
           key={item.id}
